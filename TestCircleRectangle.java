af47a95cb65bc3c799189f69d1a93d8f5fa6f720
Md. Sujon <sujoncsep@gmail.com>
2026-01-01 18:52:04 +0600

    Add files via upload

---
+++ TestCircleRectangle.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package com.mycompany.simplegeometricobject;
+
+/**
+ *
+ * @author sujon
+ */
+public class TestCircleRectangle {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+       
+        CircleFromSimpleGeometricObject circle = new CircleFromSimpleGeometricObject(1);
+        System.out.println("A circle " + circle.toString());
+        System.out.println("The color is " + circle.getColor());
+        System.out.println("The radius is " + circle.getRadius());
+        System.out.println("The area is " + circle.getArea());
+        System.out.println("The diameter is " + circle.getDiameter());
+
+        RectangleFromSimpleGeometricObject rectangle = new RectangleFromSimpleGeometricObject(2, 4);
+        System.out.println("\nA rectangle " + rectangle.toString());
+        System.out.println("The area is " + rectangle.getArea());
+        System.out.println("The perimeter is " + rectangle.getPerimeter());
+    }
+    
+}
